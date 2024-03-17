c06a2bba4bfc032817ec6ca15cddef0c8a11e71b
Petrichor <572752189@qq.com>
2024-03-17 13:23:27 +0800

    完善登录功能

--- app/service/user/login.js
+++ app/service/user/login.js
@@ -10,7 +10,7 @@ class LoginService extends Service {
     const { ctx, app } = this;
     const { username, password } = ctx.request.body;
     // // 根据用户名查询用户信息
-    const user = await ctx.model.User.findOne({ where: { username } });
+    const user = await ctx.model.User.findOne({ where: { username }, raw: true });
     if (!user) {
       ctx.status = 401;
       ctx.body = { code: 401, message: '用户名不存在' };
@@ -25,8 +25,10 @@ class LoginService extends Service {
     }
     // 签发 Token
     const token = app.jwt.sign({ userId: user.id }, app.config.jwt.secret, { expiresIn: '1d' });
+    const filteredUserInfo = { ...user };
+    delete filteredUserInfo.password;
     ctx.status = 200;
-    ctx.body = { code: 200, message: '登陆成功' };
+    ctx.body = { code: 200, message: '登陆成功，正在跳转', data: { userInfo: filteredUserInfo } };
     ctx.cookies.set('token', token, {
       httpOnly: false, // 仅允许服务器读取 Cookie
       signed: true, // 对 Cookie 进行签名
